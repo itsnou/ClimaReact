5234884880940179d98f8639d44423a396f5117f
itsnou <emiliano.meneses@gmail.com>
2020-11-19 14:40:25 -0300

    agregar header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Clima React</h1>
+    <Fragment>
+      <Header
+        titulo='Clima React App'
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              1
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
