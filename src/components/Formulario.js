b9b380411af8846542d1a308cef6f4c934a4cc8d
itsnou <emiliano.meneses@gmail.com>
2020-11-19 14:56:49 -0300

    agregando paises

---
+++ src/components/Formulario.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+const Formulario = () =>{
+
+    // state del formulario
+
+    return(
+        <form>
+            <div className="input-field col 12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                />
+
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <select
+                    name="pais"
+                    id="pais"
+                >
+                    <option value="">-- Seleccione un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+                <label htmlFor="pais">Pais: </label>
+            </div>
+        </form>
+    );
+}
+
+export default Formulario;
